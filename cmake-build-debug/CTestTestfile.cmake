0b9e9a2f018cc0b52cd168058cdff3f9126f7df6
BootyAss <panda_canniball@ail.ru>
2020-01-13 01:30:04 +0300

    checks upd

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/panda/CLionProjects/BootyAss-lab-01-parser-new
-# Build directory: C:/Users/panda/CLionProjects/BootyAss-lab-01-parser-new/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "C:/Users/panda/CLionProjects/BootyAss-lab-01-parser-new/cmake-build-debug/tests.exe")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/panda/CLionProjects/BootyAss-lab-01-parser-new/CMakeLists.txt;53;add_test;C:/Users/panda/CLionProjects/BootyAss-lab-01-parser-new/CMakeLists.txt;0;")
